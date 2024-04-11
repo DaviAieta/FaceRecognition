ff15c963f838b485056d15dfdc659ea72cdad7c9
DaviAieta <aieta.davi@gmail.com>
2024-04-11 16:59:54 -0300

    update

--- app.py
+++ app.py
@@ -5,3 +5,23 @@ import mediapipe as mp
 webcam = cv2.VideoCapture(0)
 face_recognition = mp.solutions.face_detection.FaceDetection()
 draw = mp.solutions.drawing_utils
+
+while True:
+    # Read webcam infos
+    checker, image = webcam.read() # 2 answers, checker(bool) and image
+    if not checker:
+        break
+    
+    # Recognition faces
+    list_face = face_recognition.process(image)
+    
+    if list_face.detection:
+        for face in list_face.detection:
+            draw.draw_detection(image, face)
+        cv2.imgshow('Faces:', image)
+    
+    # When press ESC, quit app
+    if cv2.waitKey(5) == 27:
+        break
+    
+webcam.release()
